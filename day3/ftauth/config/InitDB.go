80da52d5231f43ebd7d537022ed9c2666347ac84
一粒方糖（安徽）科技有限公司 <admin@asugar.cn>
2023-01-21 20:03:27 +0800

    方糖卡密授权系统

--- day3/ftauth/config/InitDB.go
+++ day3/ftauth/config/InitDB.go
@@ -6,7 +6,7 @@ import (
 )
 
 func Start() (db *gorm.DB, err error) {
-	db, err = gorm.Open(mysql.Open("root:XNXxnx520@@tcp(gz-cynosdbmysql-grp-lbda0189.sql.tencentcdb.com:27351)/ft-soft?charset=utf8"), &gorm.Config{
+	db, err = gorm.Open(mysql.Open("root:XNXxnx520@@tcp(gz-cynosdbmysql-grp-lbda0189.sql.tencentcdb.com:27351)/ft-ai?charset=utf8"), &gorm.Config{
 		SkipDefaultTransaction: true, // 关闭默认事务
 		PrepareStmt:            true, // 编译预编译
 	})

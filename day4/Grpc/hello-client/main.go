308d15ab6b0518fa623ac82ca2ed981c9750f263
方糖 <admin@asugar.cn>
2023-02-08 19:43:22 +0800

    update [grpc]

--- day4/Grpc/hello-client/main.go
+++ day4/Grpc/hello-client/main.go
@@ -1,1 +1,21 @@
-package hello_client
+package main
+
+import (
+	"context"
+	service "day4/Grpc/hello-server/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
+
+func main() {
+
+	connect, _ := grpc.Dial("127.0.0.1:8888", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	defer connect.Close()
+
+	client := service.NewGetUserByIdClient(connect)
+
+	resp, _ := client.GetUserById(context.Background(), &service.UserRequest{Username: "方糖", Address: "清华大学", Age: 22})
+	log.Println(resp.GetData())
+
+}
--- day4/Grpc/hello-server/main.go
+++ day4/Grpc/hello-server/main.go
@@ -1,1 +1,36 @@
-package hello_server
+package main
+
+import (
+	"context"
+	service "day4/Grpc/hello-server/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"net/http"
+)
+
+type server struct {
+	service.UnimplementedGetUserByIdServer
+}
+
+func (s *server) GetUserById(c context.Context, req *service.UserRequest) (*service.UserResponse, error) {
+	return &service.UserResponse{Code: http.StatusOK, Msg: "成功", Data: "Rpc远程调用成功"}, nil
+}
+
+func main() {
+	// 监听端口
+	listen, _ := net.Listen("tcp", ":8888")
+
+	// 创建grpc
+	rpc := grpc.NewServer()
+
+	// 注册服务
+	service.RegisterGetUserByIdServer(rpc, &server{})
+
+	// 启动服务
+	err := rpc.Serve(listen)
+	if err != nil {
+		log.Println("启动服务失败")
+	}
+
+}
